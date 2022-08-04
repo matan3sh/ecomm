bdd4cde5f421698a9263d859fb6e18c70cfab7c3
Matan Shaviro <matanshaviro@Matans-MBP.lan>
2022-08-04 13:45:18 +0300

    Using shared modules, async script loading, sub-app execution context

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -1,22 +1,22 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 8082,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'cart',
-      filename: 'remoteEntry.js',
+      name: "cart",
+      filename: "remoteEntry.js",
       exposes: {
-        './CartShow': './src/index'
+        "./CartShow": "./src/bootstrap",
       },
-      shared: ['faker']
+      shared: ["faker"],
     }),
     new HtmlWebPackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
-  ]
-}
+  ],
+};
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -9,11 +9,11 @@ module.exports = {
   plugins: [
     new ModuleFederationPlugin({
       name: "products",
-      filename: 'remoteEntry.js',
+      filename: "remoteEntry.js",
       exposes: {
-        "./ProductsIndex": "./src/index",
+        "./ProductsIndex": "./src/bootstrap",
       },
-      shared: ['faker']
+      shared: ["faker"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
